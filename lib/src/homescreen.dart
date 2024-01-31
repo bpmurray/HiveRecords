951c0cdc46419390120eb4014e83eea799a73f4e
Brendan Murray <brendanpmurray@gmail.com>
2024-01-31 19:52:22 +0000

    First versions from flutterviz - initial commit

---
+++ lib/src/homescreen.dart
@@ -0,0 +1,24 @@
+///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
+
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+backgroundColor: Color(0xffffffff),
+body:Align(
+alignment:Alignment.center,
+child:///***If you have exported images you must have to copy those images in assets/images directory.
+Image(
+image:AssetImage("assets/images/BrendansBeez1920.jpg"),
+height:MediaQuery.of(context).size.height * 0.3,
+width:MediaQuery.of(context).size.width,
+fit:BoxFit.fitHeight,
+),
+)
+)
+;}
+}
