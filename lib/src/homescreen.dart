66a6c639c6fcad537e9e644ecb35e2296cbd6343
Brendan Murray <brendanpmurray@gmail.com>
2024-01-31 20:22:19 +0000

    Updated to be more of a splash screen

--- lib/src/homescreen.dart
+++ lib/src/homescreen.dart
@@ -2,24 +2,49 @@
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+class homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xffffffff),
-        body: Align(
+      backgroundColor: Color(0xffffffff),
+      body: Container(
+        margin: EdgeInsets.all(0),
+        padding: EdgeInsets.all(0),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 216, 216, 218),
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.zero,
+          border: Border.all(color: Color(0x4d9e9e9e), width: 1),
+        ),
+        child: Stack(
           alignment: Alignment.center,
-          child:
-
-              ///***If you have exported images you must have to copy those images in assets/images directory.
-              Image(
-            image: const AssetImage("assets/images/BrendansBeez1920.jpg"),
-            height: MediaQuery.of(context).size.height * 0.3,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.fitHeight,
-          ),
-        ));
+          children: [
+            ///***If you have exported images you must have to copy those images in assets/images directory.
+            Image(
+              image: AssetImage("assets/images/BrendansBeez1920.jpg"),
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.fitHeight,
+            ),
+            Align(
+              alignment: Alignment(0.0, 1.0),
+              child: Text(
+                "Hive Records",
+                textAlign: TextAlign.start,
+                overflow: TextOverflow.clip,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 22,
+                  color: Color(0xffffffff),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
